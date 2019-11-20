7745610670460914ecfed1984c68ce4fdd89bf6a
Alishaan Ali <alishaan64@gmail.com>
2019-11-20 11:23:53 -0500

    Finished Registration

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,9 @@
 function register() {
-    if (checkEmail()){
+    if(checkCompletion()){
+
+    if (checkEmail()) {
         var url = "https://jack-margeson-cincyhacks-2020-registration.glitch.me/register" +
-            "?array=" + document.getElementById("FirstName").value + 
+            "?array=" + document.getElementById("FirstName").value +
             "&array=" + document.getElementById("LastName").value +
             "&array=" + document.getElementById("PreferredName").value +
             "&array=" + document.getElementById("Email").value +
@@ -16,21 +18,43 @@ function register() {
             "&array=" + document.getElementById("DietaryRestrictions").value +
             "&array=" + document.getElementById("Referral").value +
             "&array=" + document.getElementById("Other").value
-        fetch(url);
-        subscribe()
+        fetch(url).then(function(response) {
+            console.log(response.ok);
+            if (response.ok) { subscribe(); }
+        });
     }
 }
-function subscribe() {
-    var url = "https://jack-margeson-cincyhacks-2020-registration.glitch.me/subscribe" +
-        "?name=" + document.getElementById("PreferredName").value + " " + document.getElementById("LastName").value + 
-        "&email=" + document.getElementById("Email").value
-    fetch(url);
-}
-function checkEmail() {
-    if (document.getElementById("Email").value == document.getElementById("CheckEmail").value){
+  }
+  function checkEmail() {
+    if (document.getElementById("Email").value == document.getElementById("CheckEmail").value) {
         return true;
     } else {
         alert("The emails that you have entered do not match.")
         return false;
     }
-}
+  }
+  function subscribe() {
+    var url = "https://jack-margeson-cincyhacks-2020-registration.glitch.me/subscribe" +
+        "?name=" + document.getElementById("PreferredName").value + " " + document.getElementById("LastName").value +
+        "&email=" + document.getElementById("Email").value
+    fetch(url).then(function(response) {
+        if (response.ok) { redirect(); }
+    });
+  }
+  function redirect() {
+    window.location.replace("/complete.html");
+  }
+  function checkCompletion(){
+      if(document.getElementById("FirstName").value && document.getElementById("LastName").value 
+      && document.getElementById("PreferredName").value && document.getElementById("Email").value 
+      && document.getElementById("PhoneNumber").value && document.getElementById("School").value
+      && document.getElementById("Grade").value && document.getElementById("Age").value
+      && document.getElementById("EEmail").value && document.getElementById("EPhoneNumber").value
+      && document.getElementById("Pronouns").value && document.getElementById("ShirtSize").value
+      && document.getElementById("Referral").value){
+       return true;   
+      }else{
+          alert("Please complete all required elements.")
+          return false;
+      }
+  }
